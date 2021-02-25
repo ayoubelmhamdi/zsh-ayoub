23514eec48fb6dc2f9fd7f44cc2711daa9fdd895
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-02-26 00:29:14 +0100

    curl +1

---
+++ curl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+sudo apt-get update -y >/dev/null 2>&1
+sudo apt-get install git -y >/dev/null 2>&1
+sudo apt-get install zsh -y >/dev/null 2>&1
+
+
+#mkdir $HOME/UBUNTU
+clear
+echo "\n\n\t "\
+     "begin install \n\n"
+
+git clone \
+    https://github.com/ayoubelmhamdi/zsh-ayoub.git  \
+    $HOME/zsh-Ayoub >/dev/null 2>&1
+
+sh $HOME/zsh-Ayoub/zsh.sh
+
+
+
+cd $HOME/UBUNTU/
+sudo rm -r $HOME/.zshrc  >/dev/null 2>&1
+sudo rm -r $HOME/.alias  >/dev/null 2>&1
+
+cp $HOME/zsh-Ayoub/.zshrc   $HOME/.zshrc  >/dev/null 2>&1
+cp $HOME/zsh-Ayoub/.alias   $HOME/.alias  >/dev/null 2>&1
+exec zsh >/dev/null 2>&1
+
+echo  "\n\n END"
